4db28fc551db11a850296a45a74f80639dc85496
29003413 <29003413@e214m08.istic.univ-rennes1.fr>
2015-01-06 15:51:33 +0100

    création projet

---
+++ tpmaven/src/main/java/fr/istic/master1/tpmaven/App.java
@@ -0,0 +1,13 @@
+package fr.istic.master1.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
