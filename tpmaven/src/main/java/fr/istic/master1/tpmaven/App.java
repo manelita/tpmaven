18546a2c732a9ae49bbf1fb2cf5a76fc257815e5
29003413 <29003413@e007m09.istic.univ-rennes1.fr>
2015-01-14 11:49:42 +0100

    1er commit

--- tpmaven/src/main/java/fr/istic/master1/tpmaven/App.java
+++ tpmaven/src/main/java/fr/istic/master1/tpmaven/App.java
@@ -1,13 +1,23 @@
 package fr.istic.master1.tpmaven;
 
 /**
- * Hello world!
- *
+ * @author 29003413
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	/**
+	 * Constructeur
+	 */
+	protected App() {
+		//quelque chose
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+	    System.out.println("Hello World!");
+	}
+	public final static int variable = 2;
+	public int fonction() {
+		return 0;
+	}
 }
